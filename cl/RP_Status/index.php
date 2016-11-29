a7368a706ccf180494363df75a193ed61c9114e5
root <root@h2441487.stratoserver.net>
2016-11-29 15:25:51 +0100

    Add codelists, rm Thumbs file, and add bootstrap file.

---
+++ cl/RP_Status/index.php
@@ -0,0 +1,57 @@
+<?php
+$codeListe = 'RP_Status';
+?>
+<html xmlns:atom="http://www.w3.org/2005/Atom">
+<head>
+<META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+<title><?php echo $codeListe; ?></title>
+<link href="https://services.interactive-instruments.de/aaa/cl/r/css/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<article class="exp">
+<div id="topmenu">
+</div>
+<h2>RP_Status</h2>
+<p>
+<span>ID:</span><a href="">http://xplan-raumordnung.de/cl/<?php echo $codeListe; ?>/</a>
+</p>
+<p>
+<span>Typ:</span>CodeList</p>
+<p>
+<span>Status:</span>G&uuml;ltig</p>
+<p>
+<span>XPlanung-Versionen:</span>4.0&nbsp;4.1&nbsp;5.0
+</p>
+<p>
+<span>Ver&ouml;ffentlicht:</span>1.11.2016</p>
+<p>
+<span>Aktualisiert:</span>1.11.2016</p>
+<p>
+<span>Modellarten:</span>alle&nbsp;<br>
+<span>&nbsp;</span>
+</p>
+<br>
+<h3>Wertearten</h3>
+<div class="tb_cont">
+<table>
+<tr>
+<th>Code</th><th>Name</th><th>Modellarten</th><th>Status</th>
+</tr>
+<tr>
+<td>1000</td><td><a href="http://xplan-raumordnung.de/cl/<?php echo $codeListe; ?>/1000">Dieser <?php echo $codeListe; ?></a></td><td>alle<br>
+</td><td>G&uuml;ltig</td>
+</tr>
+<tr>
+<td>2000</td><td><a href="http://xplan-raumordnung.de/cl/<?php echo $codeListe; ?>/2000">Jene <?php echo $codeListe; ?></a></td><td>alle<br>
+</td><td>G&uuml;ltig</td>
+</tr>
+<tr>
+<td>3000</td><td><a href="http://xplan-raumordnung.de/cl/<?php echo $codeListe; ?>/3000">sonstiger <?php echo $codeListe; ?></a></td><td>alle<br>
+</td><td>G&uuml;ltig</td>
+</tr>
+</table>
+</div>
+</article>
+</body>
+</html>
